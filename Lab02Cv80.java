063da5453efa44978e6c1d6811eba8a07f1a398c
unknown <4070132@HS222-08.vtsd.com>
2015-09-22 11:53:11 -0400

    My first commit

---
+++ Lab02Cv80.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Lab02Cv80 here.
+ * 
+ * Elise Kastner 
+ * 100 Point Version Output
+ */
+public class Lab02Cv80
+{
+        public static void main ( String args[])
+        {
+            System.out.println("Lab02C, 100 Point Version");
+            System.out.println();
+            System.out.println("First Java Program");
+            System.out.println();
+            System.out.println("Measure today's knowledge ");
+            System.out.println("by yesterday's confusion.");
+            System.out.println();
+            System.out.println("Bewilderment + Exposure = Obvious");
+            System.out.println("# ##### #####    ##### ##### # #   # ##### #####");
+            System.out.println("# #   # #   #    #   # #   # # ##  #   #   #    ");
+            System.out.println("# #   # #   #    ##### #   # # # # #   #   #####");
+            System.out.println("# #   # #   #    #     #   # # #  ##   #       #");
+            System.out.println("# ##### #####    #     ##### # #   #   #   #####");
+            System.out.println();
+
+        }
+    }        
